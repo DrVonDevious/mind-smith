92e84dc79edabaca15637e745a870e922c428c7a
somaia-khalil <sumaya_khair@live.com>
2020-04-09 15:50:27 -0500

    channel-sub controllers

---
+++ mind-smith-backend/app/controllers/subscription_controller.rb
@@ -0,0 +1,40 @@
+class SubscriptionController < ApplicationController
+    before_action :find_subscription, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @subscriptions = Subscription.all 
+    end
+
+    def show
+        render json: @subscription
+    end
+
+    def new
+
+    end
+
+    def create
+        @subscription = Subscription.create(subscription_params)
+        render json: @subscription
+    end
+
+    def edit
+
+    end
+
+   
+
+    def destroy
+        @subscription.destroy
+    end
+
+    private
+
+    def subscription_params
+        params.require(:subscription).permit(user_id , channel_id)
+    end
+
+    def find_subscription
+        @subscription = Subscription.find(params[:id])
+    end
+end
