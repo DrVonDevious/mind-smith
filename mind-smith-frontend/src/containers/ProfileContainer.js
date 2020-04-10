02753bea5a0e107787f7316b0542750b17d24542
DrVonDevious <drvondevious@gmail.com>
2020-04-10 09:48:09 -0500

    Add condition rendering for login/register and pages

---
+++ mind-smith-frontend/src/containers/ProfileContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProfileHeader from '../components/ProfileHeader'
+import ProfilePostsContainer from './ProfilePostsContainer'
+
+const ProfileContainer = () => {
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
+
+export default ProfileContainer
