2299a3a13ee08bc53b7f44377771c4107014ee52
somaia-khalil <sumaya_khair@live.com>
2020-04-10 10:38:05 -0500

    useState

--- mind-smith-frontend/src/App.js
+++ mind-smith-frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from './components/LoginForm'
 import Register from './components/Register'
+import Channels from './components/Channels'
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div >
       <Login/>
       <Register/>
+      <Channels/>
     </div>
   );
 }
