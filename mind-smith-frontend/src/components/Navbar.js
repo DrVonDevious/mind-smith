718cf70f8c354e78c8e69fc4354ccc15bb8c1b17
DrVonDevious <drvondevious@gmail.com>
2020-04-13 10:13:40 -0500

    Fix styling on navbar

--- mind-smith-frontend/src/components/Navbar.js
+++ mind-smith-frontend/src/components/Navbar.js
@@ -4,19 +4,21 @@ import './Navbar.css'
 
 const Navbar = (props) => {
   return (
-
-
     <div className="navbar">
       <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
       <button className="navbar-brand" onClick={() => props.handleChangePage("home")}>MindSmith</button>
-      <NavbarSearch />
       <div className="navbar-right">
-        <button className="navbar-link">{props.currentUser&&` Welcome ${props.currentUser.username}`}</button>
+        <NavbarSearch />
+        {/* <button className="navbar-link">{props.currentUser&&` Welcome ${props.currentUser.username}`}</button> */}
         {props.currentUser && <button onClick={props.handleLogout} className="navbar-link" >Logout</button>}
         {!props.currentUser && <button className="navbar-link" onClick={() => props.handleLoginRegister("login")}>Login</button>}
         {!props.currentUser &&<button className="navbar-link" onClick={() => props.handleLoginRegister("register")}>Register</button>}
         <button className="navbar-link" onClick={() => props.handleChangePage("channels")}>Channels</button>
-        {props.currentUser && <img className="navbar-profile" src='https://edsurge.imgix.net/uploads/post/image/12176/coding-1556754232.jpg?auto=compress%2Cformat&w=640&h=259&fit=crop' />}
+        {props.currentUser && 
+          <img className="navbar-profile" 
+               src='https://edsurge.imgix.net/uploads/post/image/12176/coding-1556754232.jpg?auto=compress%2Cformat&w=640&h=259&fit=crop'
+               onClick={() => props.handleChangePage('profile')}/>
+        }
       </div>
     </div>
   )
