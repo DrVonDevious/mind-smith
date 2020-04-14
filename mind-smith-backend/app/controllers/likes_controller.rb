130f7c8543c33f02e89fbe6a06c47cbac7272228
DrVonDevious <drvondevious@gmail.com>
2020-04-13 20:06:50 -0500

    Add likes model

---
+++ mind-smith-backend/app/controllers/likes_controller.rb
@@ -0,0 +1,37 @@
+class LikesController < ApplicationController
+    before_action :find_like, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @likes = Likes.all 
+        render json: @likes
+    end
+
+    def show
+        render json: @likes
+    end
+
+    def new
+    end
+
+    def create
+        @like = Like.create(like_params)
+        render json: @like
+    end
+
+    def edit
+    end
+
+    def destroy
+        @like.destroy
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
+
+    def find_like
+        @like = Like.find(params[:id])
+    end
+end
